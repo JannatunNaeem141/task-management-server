d158e41aa5e3a520d31dcabe6af52677af6c3bb7
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-07-01 12:27:29 +0600

    get api created

--- index.js
+++ index.js
@@ -9,7 +9,34 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://task_manager:<password>@cluster0.cvtib.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cvtib.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('task_manager').collections('tasks');
+
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+        // post
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from task manager')
